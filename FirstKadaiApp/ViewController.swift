e052c75aeb0996b47aa5133748bac5c70494261c
takuya.matsuhashi <sieben2012@yahoo.co.jp>
2020-12-30 17:53:22 +0900

    ストーリーボード　テキスト移動

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 松橋拓哉 on 2020/12/30.
+//  Copyright © 2020 松橋拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
